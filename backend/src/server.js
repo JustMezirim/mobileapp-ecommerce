6b3d6ee6bcf5efdd63f05455ba1b03dd07efedce
mezirim <181856138+JustMezirim@users.noreply.github.com>
2025-12-09 11:10:58 +0100

    update: script deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,10 +12,10 @@ app.get("/api/health", (req, res) => {
 
 // make our app ready for deployment
 if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../admin/dist")))
+    app.use(express.static(path.join(__dirname, "admin/dist")))
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../admin/", "dist", "index.html"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "admin", "dist", "index.html"))
     })
 }
 
