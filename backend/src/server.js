4f7d577e3b239c9cd0c945dc1a7d71b5d0592b1e
mezirim <181856138+JustMezirim@users.noreply.github.com>
2025-12-09 11:34:58 +0100

    admin paths

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,10 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { ENV } from './config/env.js';
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.get("/api/health", (req, res) => {
     res.status(200).json({message:"success"})
 })
-
-// make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-    const adminPath = path.join(__dirname, "../../admin/dist");
-    app.use(express.static(adminPath));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(adminPath, "index.html"));
-    })
-}
 
 app.listen(ENV.PORT, () => console.log("Server is up and running "))
